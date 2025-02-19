7afea11b4815739947146de4ac67f1e45f724582
Miuponn <kellygao@live.ca>
2025-02-18 19:47:32 -0500

    fixed data formatting

--- src/data/faqsData.js
+++ src/data/faqsData.js
@@ -2,44 +2,41 @@ export const faqsData = [
     {
         id: 1,
         question: "What payment methods do you accept?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 2,
         question: "How much is the shipping fee and which courier will you use?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 3,
         question: "When will I receive my order?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 4,
         question: "The item I want is out of stock. Will it come back?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 5,
         question: "My order is damaged/incorrect/never arrived.",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 6,
         question: "Do you accept returns, exchanges and cancellations?",
-        answer: "All items are “final sale” (non-returnable and non-exchangeable) once received. Returns and exchanges are not accepted. Cancellation requests are accepted within 6 hours of purchase. Please refer to Refund Policy for more details."
+        answer: "All items are \"final sale\" (non-returnable and non-exchangeable) once received. Returns and exchanges are not accepted. Cancellation requests are accepted within 6 hours of purchase. Please refer to Refund Policy for more details."
     },
     {
         id: 7,
         question: "Do you take custom commissions?",
         answer: "Depending on the scope and details of your request, yes! Just fill out my contact form with your commision inquiry."
-
     },
     {
-        id: 6,
+        id: 8,
         question: "Have any other requests?",
-        answer: "Please feel free to send me an email using our Contact page!",
-    },
-    
-    
+        answer: "Please feel free to send me an email using our Contact page!"
+    }
 ];
